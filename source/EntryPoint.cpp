43863274979ab90b0cc441fb526ffa4ff9b6aa55
Lactozilla <jp6781615@gmail.com>
2026-08-16 20:59:09 -0300

    Remove Win32 specific stuff from EntryPoint.cpp

--- source/EntryPoint.cpp
+++ source/EntryPoint.cpp
@@ -1,9 +1,5 @@
 #include <Main.h>
 
-#if defined(WIN32) && !defined(WINDOWS_CONSOLE_APP) && !defined(_MSC_VER)
-int WinMain(int argc, char* argv[]) {
-#else
 int main(int argc, char* argv[]) {
-#endif
 	return HSLMain(argc, argv);
 }
